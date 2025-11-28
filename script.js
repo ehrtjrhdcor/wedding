1d7dde34c2d22b50d5c911ba248190f106aeb12f
rhkd8567-svg <rhkd8567@gmail.com>
2025-11-28 17:06:25 +0900

    Add files via upload
    ㅁㄴㅇㄴ

--- script.js
+++ script.js
@@ -1319,7 +1319,7 @@ function shareKakao() {
         content: {
             title: '장원석 ♡ 김한비 결혼식에 초대합니다.',
             description: '2026년 1월 11일 일요일 오전 11시 \n순천아모르웨딩컨벤션 1층 엘르홀',
-            imageUrl: 'https://ehrtjrhdcor.github.io/wedding/images/thumbnail.jpg',
+            imageUrl: 'https://ehrtjrhdcor.github.io/wedding/images/썸네일.jpg',
             link: {
                 mobileWebUrl: 'https://ehrtjrhdcor.github.io/wedding/',
                 webUrl: 'https://ehrtjrhdcor.github.io/wedding/',
